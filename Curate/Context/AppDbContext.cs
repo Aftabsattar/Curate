b6b1e7a9359dfc59d74d161d3717f8e8615d7ed1
Muhammad Aftab <71002@gcslahore.edu.pk>
2025-09-18 02:17:05 +0500

    Laid out the code implementation for entity mapping

---
+++ Curate/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Curate.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Curate.Contex
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option) { }
+        public DbSet<User> user { get; set; }
+    }
+}
---
+++ Curate/Controllers/CurateController.cs
@@ -0,0 +1,32 @@
+﻿using Curate.Contex;
+using Curate.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Curate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurateController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        public CurateController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(User user) 
+        {
+            await _dbContext.user.AddAsync(user);
+            _dbContext.SaveChanges();
+            return Ok("User Added Successfully");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var user = await _dbContext.user.ToListAsync();
+            return Ok(user);
+        }
+    }
+}
